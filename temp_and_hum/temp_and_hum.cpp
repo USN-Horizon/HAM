600db6615c38989b52fa6b42af03a6fb2f74d6c4
Leon <leonbrattlihansen@gmail.com>
2024-02-28 16:31:47 +0100

    la til i tempandhum.cpp

--- temp_and_hum/temp_and_hum.cpp
+++ temp_and_hum/temp_and_hum.cpp
@@ -1,8 +1,28 @@
-#include "temp.hpp"
+#include "temp_and_hum.hpp"
 #include "hum.hpp"
 
 void t_h_loop()
 {
+    /*
+    printer << "Temperature CRC: " << HAM::crc8(receive[0], receive[1], receive[2]) << "\r\n"; 
+    // printer % HAM::PrintType::Log << "Hello" << "\r\n";
+    float tempRaw = (receive[0] << 8) + (receive[1] << 0);
+    float temp = tempRaw;
+    printer % HAM::PrintType::Log << "Temp = " << (int)temp << "\r\n";
+    static HAM::byte i = 0;
+    printer << i;
+    i++;
+    
+    // for (auto& currentByte: receive)
+    // {
+    //   printer % HAM::PrintType::Log << "Receive = " << currentByte << "\r\n";
+    // }
 
+    // for(int i = 0; i<20; i++)
+    // {
+    //   HAL_I2C_Master_Transmit(&hi2c1, 0x01, &b, 1, HAL_MAX_DELAY);
+    // }
+    HAL_Delay(1000);
+    */
 }
 
