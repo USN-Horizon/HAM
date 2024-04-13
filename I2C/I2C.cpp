64b4e7f58e7bc8c324830785fa7e9784ef31fb7f
Oess98 <oess-98@hotmail.com>
2024-04-13 17:32:41 +0200

    Updated signatures of I2C

--- I2C/I2C.cpp
+++ I2C/I2C.cpp
@@ -1,6 +1,4 @@
 #include "I2C.hpp"
-#include <Uart.hpp>
-#include <Debug.hpp>
 
 using namespace HAM;
 
@@ -14,13 +12,21 @@ m_handle{I2CDefinition}
         //Error handling
       }
 }
-bool I2C::Write(const byte& address, byte& bytes, const size_t& size)
+bool I2C::Write(const byte& address, std::vector<byte>& bytes)
 {
-    return HAL_I2C_Master_Transmit(m_handle, address, &bytes, static_cast<uint16_t>(size), HAL_MAX_DELAY) == HAL_OK;
+    return Write(address, bytes.data(), bytes.size());
 }
-bool I2C::Read(const byte& address, byte& bytes, const size_t& size)
+bool I2C::Write(const byte& address, byte* bytes, const size_t& size)
 {
-    return HAL_I2C_Master_Receive(m_handle, address, &bytes, size, HAL_MAX_DELAY) == HAL_OK;
+    return HAL_I2C_Master_Transmit(m_handle, address, bytes, static_cast<uint16_t>(size), HAL_MAX_DELAY) == HAL_OK;
+}
+bool I2C::Read(const byte& address, byte* bytes, const size_t& size)
+{
+    return HAL_I2C_Master_Receive(m_handle, address, bytes, size, HAL_MAX_DELAY) == HAL_OK;
+}
+bool I2C::Read(const byte& address, std::vector<byte>& bytes)
+{
+    return Read(address, bytes.data(), bytes.size());
 }
 
 
--- I2C/I2C.hpp
+++ I2C/I2C.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include<i2c.h>
 #include <HAMTypes.hpp>
+#include <Debug.hpp>
+#include <vector>
+#include <array>
 namespace HAM
 {
     class I2C
@@ -9,8 +12,10 @@ namespace HAM
         I2C_HandleTypeDef* m_handle{nullptr};
     public:
         I2C(I2C_HandleTypeDef* I2CDefinition);
-        bool Write(const byte& address, byte& bytes, const size_t& size);
-        bool Read(const byte& address, byte& bytes, const size_t& size);
+        bool Write(const byte& address, std::vector<byte>& bytes);
+        bool Write(const byte& address, byte* bytes, const size_t& size);
+        bool Read(const byte& address, byte* bytes, const size_t& size);
+        bool Read(const byte& address, std::vector<byte>& bytes);
         ~I2C();
     };
 
