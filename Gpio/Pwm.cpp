26e74920b2b823d38903516468ac7ed13801c7ee
varmesokker <94365938+varmesokker@users.noreply.github.com>
2024-07-23 13:23:10 +0200

    Added updated PWM

--- Gpio/Pwm.cpp
+++ Gpio/Pwm.cpp
@@ -5,10 +5,14 @@ Pwm::Pwm(const GpioPortPointer& gpioPort,  const GpioPin& pin,
             TimerHandle timer, PwmChannel channel):
             Gpio{gpioPort, pin}, m_timerHandle{timer}, m_channel{channel}
 {
-    
+    HAL_TIM_PWM_Start(&m_timerHandle, m_channel); // Maybe this should be put in the constructor?
 }
 
 void Pwm::SetPwm(const byte& value)
 {
-    HAL_TIM_PWM_Start(&m_timerHandle, m_channel);
+    
+    __HAL_TIM_SET_COMPARE(&m_timerHandle, m_channel, value); // This?
 }
+
+
+
